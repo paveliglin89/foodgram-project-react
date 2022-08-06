390dfc07162d77fb09abad0c0eb3f93ee5da71ae
Pavel Iglin <p.v.iglin.samgtu@gmail.com>
2022-08-06 17:41:23 +0400

    Second part

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*'), '*']
 
