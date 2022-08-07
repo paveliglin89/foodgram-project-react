bfb5ab70e979a4b74819160ff50ab4e26a737dcf
Pavel Iglin <p.v.iglin.samgtu@gmail.com>
2022-08-07 13:08:51 +0400

    Second part

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('api/', include(api_patterns)),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
+urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
